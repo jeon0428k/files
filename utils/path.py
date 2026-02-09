d4044665fc996d3d4b6efcab3359d32a0d585e8c
jeon0428k <jeon0428k@gmail.com>
2026-02-09 00:37:33 +0000

    .

--- utils/path.py
+++ utils/path.py
@@ -1,20 +1,15 @@
 from pathlib import Path
 
 
-def get_workspace_root() -> Path:
+def get_root() -> Path:
     """
-    워크스페이스 루트 반환
-    기준:
-      D:/program/deploy/run_all.py
-      D:/program/work.py
-      D:/program/check.py
-    → 모두 D:/program 을 루트로 간주
+    ROOT 반환
     """
     return Path(__file__).resolve().parents[1]
 
 
 def resolve_path(*paths: str) -> Path:
     """
-    워크스페이스 기준 절대경로 생성
+    ROOT 기준 절대경로 생성
     """
-    return get_workspace_root().joinpath(*paths)
+    return get_root().joinpath(*paths)
