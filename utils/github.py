b0e2963799fd71857f6d1b0d7e554875bcc75486
jeon0428k <jeon0428k@gmail.com>
2026-03-04 04:24:50 +0000

    .

--- utils/github.py
+++ utils/github.py
@@ -3,7 +3,7 @@ import json
 import yaml
 import requests
 import subprocess
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 from urllib.parse import urlencode, urlsplit, urlunsplit, parse_qsl
 
 
